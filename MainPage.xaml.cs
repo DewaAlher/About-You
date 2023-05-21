9a76a31d920aa8ee7261e2beac66811feb60f15f
DewaAlher <59711776+DewaAlher@users.noreply.github.com>
2023-05-21 15:47:26 +0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DataDiriApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string nama = txtNama.Text;
+            string umur = txtUmur.Text;
+            string alamat = txtAlamat.Text;
+            string hobi = txtHobi.Text;
+            string band = txtBand.Text;
+            string lagu = txtLagu.Text;
+
+            string output = "Data Diri:\n" +
+                            "Nama: " + nama + "\n" +
+                            "Umur: " + umur + "\n" +
+                            "Alamat: " + alamat + "\n" +
+                            "Hobi: " + hobi + "\n" +
+                            "Band Favorit: " + band + "\n" +
+                            "Lagu Favorit: " + lagu;
+
+            txtOutput.Text = output;
+        }
+    }
+}
